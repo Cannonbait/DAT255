7cb6934578a14cd53d439e1ad256c6eb86699ff4
Elvira Jonsson <elviraj@student.chalmers.se>
2014-09-25 14:32:06 +0200

    Created AutomotiveManager in mainactivity

--- project/app/src/main/java/edu/chalmers/pickuapp/app/MainActivity.java
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/MainActivity.java
@@ -1,9 +1,18 @@
 package edu.chalmers.pickuapp.app;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.swedspot.automotiveapi.AutomotiveSignal;
+import android.swedspot.automotiveapi.AutomotiveSignalId;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.swedspot.automotiveapi.AutomotiveFactory;
+import com.swedspot.automotiveapi.AutomotiveListener;
+import com.swedspot.vil.distraction.DriverDistractionLevel;
+import com.swedspot.vil.distraction.DriverDistractionListener;
+import com.swedspot.vil.policy.AutomotiveCertificate;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +21,37 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new AsyncTask(){
+
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                AutomotiveFactory.createAutomotiveManagerInstance(new AutomotiveCertificate(new byte[0]),
+
+                        new AutomotiveListener() {
+                            @Override
+                            public void receive(AutomotiveSignal automotiveSignal) {
+                                Log.i("PickUApp", "Signal recieved");
+                            }
+
+                            @Override
+                            public void timeout(int i) {
+
+                            }
+
+                            @Override
+                            public void notAllowed(int i) {
+
+                            }
+                        }, new DriverDistractionListener() {
+                    @Override
+                    public void levelChanged(DriverDistractionLevel driverDistractionLevel) {
+                        Log.i("PickUApp", "Level changed");
+                    }
+                }).register(AutomotiveSignalId.FMS_CURRENT_GEAR);
+                return null;
+            }
+        }.execute();
     }
 
 
