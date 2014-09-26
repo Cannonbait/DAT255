7d4454f59322d9bb4d8a48d4896a09671dfa2850
Joakim Thorén <joakimthoren93@gmail.com>
2014-09-26 19:45:48 +0200

    fixed hardcoded paths in build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-gradle build -p ~/Programming/gitrepos/DAT255/project/ && adb install -r ~/Programming/gitrepos/DAT255/project/app/build/apk/app-debug-unaligned.apk
+#Build script for PickUApp
+
+echo "NOTE: You must cd to git root folder (eg MyGitRepos/DAT255) when running build.sh"
+gradle build -p project/ && adb install -r project/app/build/apk/app-debug-unaligned.apk
