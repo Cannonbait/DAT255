31a7e9261372e4a77c175fce10cfcf94c170321f
Joakim Thorén <joakimthoren93@gmail.com>
2014-09-29 18:25:46 +0200

    added some elaborating comments to DriverSetRoute

--- project/app/src/main/java/edu/chalmers/pickuapp/app/model/DriverSetRoute.java
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/model/DriverSetRoute.java
@@ -1,5 +1,6 @@
 package edu.chalmers.pickuapp.app.model;
 
+import edu.chalmers.pickuapp.app.events.*;
 
 class DriverSetRoute extends Sequence {
 	
@@ -7,13 +8,24 @@ class DriverSetRoute extends Sequence {
 		super();
 	}
 
-	public void register() {
+	public void onRegister() {
 
-	};
+	}
 	public Sequence execute() {
+		//Sleep until ok-button
+		//Then return appropiate sequence, for now return MockSequence
+		return getSequence(MockSequence.class);
+	}
+	public void onUnregister() {
 
-	};
-	public void unregister() {
+	}
 
-	};
+	public void onEvent(Event e) {
+		
+		//ON NEW MAP LOCATION SELECTED-event
+		//Save google-maps location data to variable
+		
+		//ON OK BUTTON
+		//unsleep main thread, which will resume execute()
+	}
 }
