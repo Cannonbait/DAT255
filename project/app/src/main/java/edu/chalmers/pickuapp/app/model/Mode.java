297b9cdcc422330c83ee6645a0ea63c739d35e74
Elvira Jonsson <elviraj@student.chalmers.se>
2014-09-29 16:08:45 +0200

    Added Mode class

---
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/model/Mode.java
@@ -0,0 +1,21 @@
+package edu.chalmers.pickuapp.app.model;
+
+/**
+ * Created by elvirajonsson on 2014-09-29.
+ */
+public class Mode extends Sequence {
+    @Override
+    public void register() {
+        
+    }
+
+    @Override
+    public Sequence execute() {
+        return null;
+    }
+
+    @Override
+    public void unregister() {
+
+    }
+}
