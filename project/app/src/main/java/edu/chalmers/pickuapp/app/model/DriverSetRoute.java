854d987f614378cfe1d7794b66b525706c1ea15f
Joakim Thorén <joakimthoren93@gmail.com>
2014-09-29 16:18:54 +0200

    created stub DriverSetRoute

---
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/model/DriverSetRoute.java
@@ -0,0 +1,19 @@
+package edu.chalmers.pickuapp.app.model;
+
+
+class DriverSetRoute extends Sequence {
+	
+	public DriverSetRoute() {
+		super();
+	}
+
+	public void register() {
+
+	};
+	public Sequence execute() {
+
+	};
+	public void unregister() {
+
+	};
+}
