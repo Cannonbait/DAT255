fcc2bfb9dd0e16c0d9217ec380bdbf5a500a01e3
Malin <malin.thelin12@gmail.com>
2014-09-30 15:43:57 +0200

    Added some empty methods to hitchhikeretroute

--- project/app/src/main/java/edu/chalmers/pickuapp/app/model/HitchhikerSetRoute.java
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/model/HitchhikerSetRoute.java
@@ -1,7 +1,35 @@
 package edu.chalmers.pickuapp.app.model;
 
+import edu.chalmers.pickuapp.app.events.*;
+
 /**
  * Created by Malin on 2014-09-30.
  */
-public class HitchhikerSetRoute {
+public class HitchhikerSetRoute extends Sequence {
+
+    public HitchhikerSetRoute() {
+        super();
+    }
+
+    public void onRegister() {
+
+    }
+    public Sequence execute() {
+        //Sleep until ok-button
+        //Then return appropiate sequence, for now return MockSequence
+        return getSequence(MockSequence.class);
+    }
+    public void onUnregister() {
+
+    }
+
+    public void onEvent(Event e) {
+
+        //ON NEW MAP LOCATION SELECTED-event
+        //Save google-maps location data to variable
+
+        //ON OK BUTTON
+        //unsleep main thread, which will resume execute()
+    }
+
 }
