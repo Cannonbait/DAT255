88a5b1530d064f9a2b6f601c22aab3e61a49dd5d
Joakim Thorén <joakimthoren93@gmail.com>
2014-09-30 18:06:59 +0200

    created DriverSetRoute-event

--- project/app/src/main/java/edu/chalmers/pickuapp/app/events/DriverPickedRoute.java
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/events/DriverPickedRoute.java
@@ -1,8 +1,19 @@
 package edu.chalmers.pickuapp.app.events;
 
+import edu.chalmers.pickuapp.app.model.*;
+
 /**
  * Event that occurs when the driver clicks OK-button after selecting a route
  */
-class DriverPickedRoute implements Event {
+public class DriverPickedRoute implements Event {
+
+	public RouteData routeData;
+
+	/**
+	 * @param routeData what route did the driver select?
+	 */
+	public DriverPickedRoute(RouteData routeData) {
+		this.routeData = routeData;
+	}
 
 }
