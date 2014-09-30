6c89c84a70ba5ff8c0e1fb4c81193035ac8e9a7c
Joakim Thorén <joakimthoren93@gmail.com>
2014-09-30 18:05:12 +0200

    fixed #20

---
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/model/RouteData.java
@@ -0,0 +1,33 @@
+package edu.chalmers.pickuapp.app.model;
+
+public class RouteData {
+
+	private Coordinate source;
+	private Coordinate destination;
+
+	/**
+	 * @param  source      Where to pickup
+	 * @param  destination Where to go
+	 */
+	public RouteData(Coordinate source, Coordinate destination) {
+		this.source = new Coordinate(source);
+		this.destination = new Coordinate(destination);
+	}
+
+	/**
+	 * Copy-constructor
+	 * @param  routeData routeData to copy 
+	 */
+	public RouteData(RouteData routeData) {
+		this(routeData.source, routeData.destination);
+	}
+
+	public Coordinate getSource() {
+		return new Coordinate(source);
+	}
+
+	public Coordinate getDestionation() {
+		return new Coordinate(destination);
+	}
+
+}
