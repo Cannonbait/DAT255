1bee8746e5bd6cc9937dc21399d08b7298b5ef5d
Joakim Thorén <joakimthoren93@gmail.com>
2014-10-02 14:22:10 +0200

    fixed 25

--- project/app/src/main/java/edu/chalmers/pickuapp/app/model/DriverSetRoute.java
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/model/DriverSetRoute.java
@@ -3,35 +3,40 @@ package edu.chalmers.pickuapp.app.model;
 import edu.chalmers.pickuapp.app.events.*;
 import java.lang.*;
 
-class DriverSetRoute extends Sequence {
-	
+class DriverSetRoute extends Sequence implements EventListener{
+
+	private Thread myThread;
+	Sequence nextSequence = null;
+	boolean isDone = false;
+
 	public DriverSetRoute() {
 		super();
 	}
 
-	public void onRegister() {
-
-	}
-	public Sequence execute() {
-		//Sleep until ok-button
-		//Then return appropiate sequence, for now return MockSequence
-		return getSequence(MockSequence.class);
-	}
-	public void onUnregister() {
-
-	}
-
-	public void onEvent(Event e) {
+	public void processEvent(Event event) {
 		
 		//ON NEW MAP LOCATION SELECTED-event
 		//Save google-maps location data to variable
 		
 		//ON OK BUTTON
 		//unsleep main thread, which will resume execute()
-		if(e instanceof DriverPickedRoute) {
-			RouteData routeData = new RouteData(((DriverPickedRoute)e).routeData);
+		if(event instanceof DriverPickedRouteEvent) {
+			RouteData routeData = new RouteData(((DriverPickedRouteEvent)event).routeData);
 
-			//Give routeData to next sequence (matchmaker)
+			//Set nextSequence to be matchmaker
+			//Give routeData to nextSequence (matchmaker)
+			
+
+			//This sequence is done now, nothing more to do here
+			isDone = true;
 		}
 	}
+
+	public abstract Sequence getNextSequence() {
+		return nextSequence;
+	};
+    public abstract boolean isDone() {
+    	return isDone;
+    };
+
 }
