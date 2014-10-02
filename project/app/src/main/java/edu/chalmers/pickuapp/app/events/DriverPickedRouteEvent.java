8a617b1c3ae62eb45a78fad0bd0b9129559f70ef
Joakim Thorén <joakimthoren93@gmail.com>
2014-10-02 13:24:42 +0200

    renamed DriverPickedRoute to DriverPickedRouteEvent

---
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/events/DriverPickedRouteEvent.java
@@ -0,0 +1,19 @@
+package edu.chalmers.pickuapp.app.events;
+
+import edu.chalmers.pickuapp.app.model.*;
+
+/**
+ * Event that occurs when the driver clicks OK-button after selecting a route
+ */
+public class DriverPickedRouteEvent implements Event {
+
+	public RouteData routeData;
+
+	/**
+	 * @param routeData what route did the driver select?
+	 */
+	public DriverPickedRouteEvent(RouteData routeData) {
+		this.routeData = routeData;
+	}
+
+}
