38514efc5e648af225ba824500b4598e78fb50c8
Elvira Jonsson <elviraj@student.chalmers.se>
2014-10-06 16:17:48 +0200

    Added testpackage

---
+++ project/app/src/main/java/test/edu/chalmers/pickuapp/app/model/ModeTest.java
@@ -0,0 +1,17 @@
+package test.edu.chalmers.pickuapp.app.model;
+
+import edu.chalmers.pickuapp.app.model.Sequence;
+import junit.framework.*;
+
+/**
+ * Created by elvirajonsson on 2014-10-06.
+ */
+public class ModeTest extends Assert {
+
+    private boolean isDone;
+    private Sequence nextSequence;
+
+    public void testProcessEventPickedDriver(){
+
+    }
+}
