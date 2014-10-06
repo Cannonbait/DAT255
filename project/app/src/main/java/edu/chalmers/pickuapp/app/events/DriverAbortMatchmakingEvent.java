b9f7e85ab4b3572fdb9858c8bb52164c8aed29d7
Joakim Thorén <joakimthoren93@gmail.com>
2014-10-06 16:46:06 +0200

    fixed #39, implemented DriverAbortMatchmaking-event

---
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/events/DriverAbortMatchmakingEvent.java
@@ -0,0 +1,5 @@
+package edu.chalmers.pickuapp.app.events;
+
+public class DriverAbortMatchmakingEvent implements Event {
+	//No data needs to transferred to anything when aborted, hence empty event
+}
