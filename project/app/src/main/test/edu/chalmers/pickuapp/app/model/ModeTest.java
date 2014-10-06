14594366f7c2d66695c15a6c70cf7984701a2454
Elvira Jonsson <elviraj@student.chalmers.se>
2014-10-06 16:48:35 +0200

    Refactored test environment

---
+++ project/app/src/main/test/edu/chalmers/pickuapp/app/model/ModeTest.java
@@ -0,0 +1,30 @@
+package edu.chalmers.pickuapp.app.model;
+
+import junit.framework.TestCase;
+
+public class ModeTest extends TestCase {
+
+    private Mode mode;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        mode = new Mode();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testProcessEvent() throws Exception {
+
+    }
+
+    public void testGetNextSequence() throws Exception {
+
+    }
+
+    public void testIsDone() throws Exception {
+
+    }
+}
