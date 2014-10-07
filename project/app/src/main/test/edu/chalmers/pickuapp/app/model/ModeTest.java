a39f76d4d0c4a4baaef2617935d107477950236b
Elvira Jonsson <elviraj@student.chalmers.se>
2014-10-08 01:04:45 +0200

    test environment works

--- project/app/src/main/test/edu/chalmers/pickuapp/app/model/ModeTest.java
+++ project/app/src/main/test/edu/chalmers/pickuapp/app/model/ModeTest.java
@@ -1,23 +1,34 @@
 package edu.chalmers.pickuapp.app.model;
 
-import edu.chalmers.pickuapp.app.events.Event;
-import edu.chalmers.pickuapp.app.events.PickedDriverEvent;
-import junit.framework.*;
+import edu.chalmers.pickuapp.app.events.*;
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
-public class ModeTest extends TestCase {
+import static org.junit.Assert.*;
 
-    private Mode mode = new Mode();
+public class ModeTest extends Assert {
 
-    public void testProcessEventPickedDriver() throws Exception{
-        Event event = new PickedDriverEvent();
-        mode.processEvent(event);
-        assertTrue(mode.isDone());
+    private Mode mode;
+
+    @Before
+    public void setUp() throws Exception {
+        mode = new Mode();
     }
 
+    @Test
+    public void testProcessEvent() throws Exception {
+       /* Event evt = new PickedDriverEvent();
+        mode.processEvent(evt);
+        assertTrue(mode.isDone());*/
+    }
+
+    @Test
     public void testGetNextSequence() throws Exception {
 
     }
 
+    @Test
     public void testIsDone() throws Exception {
 
     }
