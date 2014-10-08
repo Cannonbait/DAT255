921aba8a4dd0e52fa800d3c0eb78319082f44079
Joakim Thorén <joakimthoren93@gmail.com>
2014-10-08 15:16:20 +0200

    fixed #48, implemented RouteDataTest

---
+++ project/app/src/main/test/edu/chalmers/pickuapp/app/model/RouteDataTest.java
@@ -0,0 +1,35 @@
+package edu.chalmers.pickuapp.app.model;
+
+import edu.chalmers.pickuapp.app.events.*;
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RouteDataTest extends Assert {
+
+    RouteData routeData;
+    Coordinate source;
+    Coordinate destination;
+
+    @Before
+    public void setUp() throws Exception {
+       source = new Coordinate(0, 0);
+       destination = new Coordinate(100, 100);
+       routeData = new RouteData(source, destination);
+    }
+
+    @Test
+    public void testEquals() {
+        RouteData nullRouteData = null;
+        RouteData differentRouteData = new RouteData(new Coordinate(5, 5), new Coordinate(6, 6));
+        RouteData identicalRouteData = new RouteData(routeData);
+        RouteData sameRouteData = routeData;
+
+        assertTrue(!routeData.equals(nullRouteData));
+        assertTrue(!routeData.equals(differentRouteData));
+        assertTrue(routeData.equals(identicalRouteData));
+        assertTrue(routeData.equals(sameRouteData));
+    }
+}
