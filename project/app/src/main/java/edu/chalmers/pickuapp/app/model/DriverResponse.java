348d498f8b58a757fd5ef5e9e07be8f2edb522cf
Francine Antiloop Mäkelä <guldhamster.hopp@hotmail.com>
2014-10-09 14:21:14 +0200

    DriverResponse implemented

--- project/app/src/main/java/edu/chalmers/pickuapp/app/model/DriverResponse.java
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/model/DriverResponse.java
@@ -1,19 +1,45 @@
 package edu.chalmers.pickuapp.app.model;
 
-import edu.chalmers.pickuapp.app.events.*;
+import edu.chalmers.pickuapp.app.events.Event;
 
-public class DriverResponse extends Sequence {
-	public DriverResponse() {
+/**
+ * Created by Ance on 2014-10-06.
+ */
+public class DriverResponse extends Sequence{
+
+	private Sequence nextSequens;
+	private boolean isDone = false;
+	private RouteData routeData;
+	private Date date;
+
+	public DriverResponse(){
 		super();
 	}
 
-
+	@Override
 	public void processEvent(Event event) {
-		//TODO: implement
+		/* TODO- waiting for event
+		if(event instanceof EventFromMockServer){
+			nextSequens = getSequence(WhateversIsNext.class);
+			RouteData routeData = new RouteData();
+			Date date = new Date(((WhateverIsNext)(event)).getRouteData()));
+			isDone = true;
+		}
+		*/
 	}
-    public Sequence getNextSequence() {
-    	//TODO: implement
-    	return null;
-    }
 
+	public void insert(RouteData routeData, Date date){
+		this.routeData = routeData;
+		this.date = date;
+	}
+
+	@Override
+	public Sequence getNextSequence() {
+		return nextSequens;
+	}
+
+	@Override
+	public boolean isDone() {
+		return isDone;
+	}
 }
