4478e35c58dd3defc12758f530e6b4b4c8782aa7
Elvira Jonsson <elviraj@student.chalmers.se>
2014-10-10 12:00:07 +0200

    Implemented test for DisplayInfo

---
+++ project/app/src/main/test/edu/chalmers/pickuapp/app/model/DisplayInfoTest.java
@@ -0,0 +1,76 @@
+package edu.chalmers.pickuapp.app.model;
+
+import edu.chalmers.pickuapp.app.events.DisplayInfoOKEvent;
+import edu.chalmers.pickuapp.app.events.DriverAbortMatchmakingEvent;
+import edu.chalmers.pickuapp.app.events.MeetupEvent;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DisplayInfoTest {
+
+    private DisplayInfo displayInfo;
+    private Model model;
+    private Coordinate meetupPoint;
+    private Date date;
+
+
+    @Before
+    public void setUp(){
+        model = new Model();
+        displayInfo = (DisplayInfo)Sequence.getSequence(DisplayInfo.class);
+        meetupPoint = new Coordinate(2.0, 3.0);
+        date = new Date(2014, 12, 3, 15, 30, 0);
+    }
+
+    @Test
+    public void testProcessEvent() throws Exception {
+        //Nothing to test
+    }
+
+    @Test
+    public void testGetNextSequence() throws Exception {
+        //Before any event
+        assertNull(displayInfo.getNextSequence());
+
+        //Wrong event
+        displayInfo.processEvent(new DriverAbortMatchmakingEvent());
+        assertNull(displayInfo.getNextSequence());
+
+        //MeetupEvent
+        displayInfo.processEvent(new MeetupEvent(meetupPoint, date));
+        assertNull(displayInfo.getNextSequence());
+
+        //DisplayInfoOKEvent
+        displayInfo.processEvent(new DisplayInfoOKEvent());
+        assertEquals(displayInfo.getNextSequence().getClass(), Mode.class);
+    }
+
+    @Test
+    public void testOnStart() throws Exception {
+        displayInfo.onStart();
+        assertTrue(!displayInfo.isDone());
+        assertNull(displayInfo.getMeetupPoint());
+        assertNull(displayInfo.getDate());
+
+    }
+
+    @Test
+    public void testGetDate() throws Exception {
+        assertNull(displayInfo.getDate());
+
+        displayInfo.processEvent(new MeetupEvent(meetupPoint, date));
+        assertEquals(displayInfo.getDate(), date);
+
+    }
+
+    @Test
+    public void testGetMeetupPoint() throws Exception {
+        assertNull(displayInfo.getMeetupPoint());
+
+        displayInfo.processEvent(new MeetupEvent(meetupPoint, date));
+        assertEquals(displayInfo.getMeetupPoint(), meetupPoint);
+
+    }
+}
