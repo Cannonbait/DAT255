2cee65ab39927ca8dd3bdaaa2058f05c7c868269
Elvira Jonsson <elviraj@student.chalmers.se>
2014-10-10 10:51:52 +0200

    Added event for getting meetupdata from server

---
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/events/MeetupEvent.java
@@ -0,0 +1,27 @@
+package edu.chalmers.pickuapp.app.events;
+
+import edu.chalmers.pickuapp.app.model.Coordinate;
+import edu.chalmers.pickuapp.app.model.Date;
+
+/**
+ * Created by elvirajonsson on 2014-10-10.
+ */
+//This event is to be trown by the server containing the data on where and when the driver and HH should meet up
+public class MeetupEvent implements Event {
+
+    private Coordinate meetupPoint;
+    private Date date;
+
+    public MeetupEvent(Coordinate meetupPoint, Date date){
+        this.meetupPoint = meetupPoint;
+        this.date = date;
+    }
+
+    public Coordinate getMeetupPoint(){
+        return new Coordinate(meetupPoint.getLatitude(), meetupPoint.getLongitude());
+    }
+
+    public Date getDate(){
+        return new Date(date);
+    }
+}
