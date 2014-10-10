23ed03994d638c592f5d0de74c55d63017a1029c
Elvira Jonsson <elviraj@student.chalmers.se>
2014-10-10 11:59:31 +0200

    Implemented DisplayInfo

--- project/app/src/main/java/edu/chalmers/pickuapp/app/model/DisplayInfo.java
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/model/DisplayInfo.java
@@ -1,24 +1,48 @@
 package edu.chalmers.pickuapp.app.model;
 
-import edu.chalmers.pickuapp.app.events.Event;
+import edu.chalmers.pickuapp.app.events.*;
 
 /**
  * Created by elvirajonsson on 2014-10-09.
  */
 public class DisplayInfo extends Sequence {
     private Sequence nextSequence;
+    private Coordinate meetupPoint = null;
+    private Date date = null;
 
     public DisplayInfo(){
         super();
     }
-
+    //How do I get the meetup data from model to view?
     @Override
     public void processEvent(Event event) {
+        if(event instanceof MeetupEvent){
+            meetupPoint = ((MeetupEvent) event).getMeetupPoint();
+            date = ((MeetupEvent) event).getDate();
+        } else if(event instanceof DisplayInfoOKEvent){
+            nextSequence = getSequence(Mode.class);
+            isDone = true;
+        }
 
     }
 
     @Override
     public Sequence getNextSequence() {
-        return null;
+        return nextSequence;
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        meetupPoint = null;
+        date = null;
+    }
+
+    public Date getDate(){
+        return date;
+    }
+
+    public Coordinate getMeetupPoint(){
+        return meetupPoint;
     }
 }
