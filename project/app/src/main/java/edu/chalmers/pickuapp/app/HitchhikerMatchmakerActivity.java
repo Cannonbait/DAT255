837fe78645aea8a89921358522ae33b95f571510
Joakim Thorén <joakimthoren93@gmail.com>
2014-10-11 15:04:55 +0200

    fixed #31, implemented HitchhikerMatchmakerActivity

---
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/HitchhikerMatchmakerActivity.java
@@ -0,0 +1,47 @@
+package edu.chalmers.pickuapp.app;
+
+import android.app.*;
+import android.os.Bundle;
+import android.text.format.*;
+import android.util.*;
+import android.view.*;
+import android.widget.*;
+import com.google.android.gms.maps.*;
+import edu.chalmers.pickuapp.app.model.*;
+import edu.chalmers.pickuapp.app.events.*;
+
+import java.util.*;
+
+
+public class HitchhikerMatchmakerActivity extends ChildActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.driver_matchmaker);
+    }
+
+
+    public void abortMatchmaking(View view) {
+        EventBus.INSTANCE.reportEvent(new AbortMatchmakingEvent());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.driver_matchmaker, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
