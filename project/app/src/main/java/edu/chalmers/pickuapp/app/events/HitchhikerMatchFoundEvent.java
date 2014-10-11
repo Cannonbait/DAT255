a246203c9ab16572b7ac7bf2baf6044e4216a1af
Joakim Thorén <joakimthoren93@gmail.com>
2014-10-11 15:54:18 +0200

    fixed #52, implemented HitchhikerMatchFoundEvent

--- project/app/src/main/java/edu/chalmers/pickuapp/app/events/HitchhikerMatchFoundEvent.java
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/events/HitchhikerMatchFoundEvent.java
@@ -1,8 +1,24 @@
 package edu.chalmers.pickuapp.app.events;
 
+import edu.chalmers.pickuapp.app.events.*;
+import edu.chalmers.pickuapp.app.model.*;
+
 /**
  * Created by elvirajonsson on 2014-10-09.
+ * Revised by Joakim Thorén on 2014-10-11.
  */
 public class HitchhikerMatchFoundEvent implements Event {
+
+	//Save whatever data required for displaying info about match
+	//I.E: Time, location, Name of driver and hitchhiker etcetc
+	
+	public RouteData routeData;
+	public Date date;
+
+	//Pass that data to this constructor
+	public HitchhikerMatchFoundEvent(RouteData routeData, Date date) {
+		this.routeData = new RouteData(routeData);
+		this.date = new Date(date);
+	}
 
 }
