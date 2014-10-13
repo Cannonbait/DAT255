406039ce5989a0a3742cf0728cc61fbbecb1119b
Joakim Thorén <joakimthoren93@gmail.com>
2014-10-13 16:43:23 +0200

    fixed #62, implemented HitchhikerAccept-event

---
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/events/HitchhikerAcceptEvent.java
@@ -0,0 +1,10 @@
+package edu.chalmers.pickuapp.app.events;
+
+import edu.chalmers.pickuapp.app.model.*;
+
+/**
+ * Event that occurs when the hitchhiker clicks the "DeclineAndContinue"-button
+ */
+class HitchhikerAcceptEvent {
+	//No data is required
+}
