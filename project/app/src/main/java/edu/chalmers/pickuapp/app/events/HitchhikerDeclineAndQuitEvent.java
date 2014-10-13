810d68bd1b3048b638def22dbd124ec2bb8d23f5
Joakim Thorén <joakimthoren93@gmail.com>
2014-10-13 17:36:04 +0200

    HitchhikerDeclineMatchAndQuit implements Event

--- project/app/src/main/java/edu/chalmers/pickuapp/app/events/HitchhikerDeclineAndQuitEvent.java
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/events/HitchhikerDeclineAndQuitEvent.java
@@ -5,6 +5,6 @@ import edu.chalmers.pickuapp.app.model.*;
 /**
  * Event that occurs when the hitchhiker clicks the "DeclineAndQuit"-button
  */
-public class HitchhikerDeclineAndQuitEvent {
+public class HitchhikerDeclineAndQuitEvent implements Event {
 	//No data is required
 }
