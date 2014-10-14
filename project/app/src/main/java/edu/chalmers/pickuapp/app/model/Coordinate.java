93fb84d43d588a7ebfd22a7fc5f8c4ec64541749
Cannonbait <ivar_busig@hotmail.com>
2014-10-14 23:07:03 +0200

    Fixed bugg in setters

--- project/app/src/main/java/edu/chalmers/pickuapp/app/model/Coordinate.java
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/model/Coordinate.java
@@ -33,9 +33,9 @@ public class Coordinate {
 		return longitude;
 	}
 
-    public void setLatitude() { this.latitude = latitude; }
+    public void setLatitude(double latitude) { this.latitude = latitude; }
 
-    public void setLongitude() { this.longitude = longitude; }
+    public void setLongitude(double longitude) { this.longitude = longitude; }
 
 	@Override
 	public boolean equals(Object rhs) {
