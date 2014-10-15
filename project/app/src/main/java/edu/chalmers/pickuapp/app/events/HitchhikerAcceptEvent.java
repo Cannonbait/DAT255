863eda18c7b157168a7d2408a057fac020c437c8
Joakim Thorén <joakimthoren93@gmail.com>
2014-10-15 13:11:21 +0200

    fixed erroneous comment in HitchhikerAcceptEvent

--- project/app/src/main/java/edu/chalmers/pickuapp/app/events/HitchhikerAcceptEvent.java
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/events/HitchhikerAcceptEvent.java
@@ -3,7 +3,8 @@ package edu.chalmers.pickuapp.app.events;
 import edu.chalmers.pickuapp.app.model.*;
 
 /**
- * Event that occurs when the hitchhiker clicks the "DeclineAndContinue"-button
+ * Event that occurs when the hitchhiker clicks the "Accept Driver"-button
+ * 
  */
 public class HitchhikerAcceptEvent implements Event {
 	//No data is required
