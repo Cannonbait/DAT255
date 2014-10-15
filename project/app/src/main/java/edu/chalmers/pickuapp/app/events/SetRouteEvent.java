ffb45769b242b349f2626dc6b6599fb7ea899b33
Cannonbait <ivar_busig@hotmail.com>
2014-10-15 14:30:22 +0200

    Refactored constructor and added getter method

--- project/app/src/main/java/edu/chalmers/pickuapp/app/events/SetRouteEvent.java
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/events/SetRouteEvent.java
@@ -2,22 +2,17 @@ package edu.chalmers.pickuapp.app.events;
 
 import edu.chalmers.pickuapp.app.model.Coordinate;
 import edu.chalmers.pickuapp.app.model.Date;
+import edu.chalmers.pickuapp.app.model.RouteData;
 
 /**
  * Created by cannonbait on 2014-10-13.
  */
 public class SetRouteEvent implements Event{
-    private final Date fromDate;
-    private final Date toDate;
-    private final Coordinate fromCoordinates;
-    private final Coordinate toCoordinates;
-
-    public SetRouteEvent(Date fromDate, Date toDate, Coordinate fromCoordinates, Coordinate toCoordinates){
-        this.fromDate = fromDate;
-        this.toDate = toDate;
-        this.fromCoordinates = fromCoordinates;
-        this.toCoordinates = toCoordinates;
+    private final RouteData route;
+    public SetRouteEvent(RouteData route){
+        this.route = route;
     }
+    public RouteData getRoute(){ return route; }
 
 
 }
