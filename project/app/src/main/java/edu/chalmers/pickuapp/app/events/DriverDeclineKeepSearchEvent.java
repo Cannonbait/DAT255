34e18206f73d73cc7cbbe40544e23ed3a57d488f
Francine Antiloop Mäkelä <guldhamster.hopp@hotmail.com>
2014-10-15 17:25:41 +0200

    Renamed event

---
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/events/DriverDeclineKeepSearchEvent.java
@@ -0,0 +1,8 @@
+package edu.chalmers.pickuapp.app.events;
+
+/**
+ * Event sent when a driver don't want to pick up a suggested hitchhiker.
+ */
+public class DriverDeclineKeepSearchEvent implements Event{
+
+}
