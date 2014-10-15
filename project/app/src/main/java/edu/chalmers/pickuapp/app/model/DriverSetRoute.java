197dabf08f070ab7a0055604416d28f54f130802
Cannonbait <ivar_busig@hotmail.com>
2014-10-15 14:30:51 +0200

    Cleanup in preparation for refactoring

--- project/app/src/main/java/edu/chalmers/pickuapp/app/model/DriverSetRoute.java
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/model/DriverSetRoute.java
@@ -1,5 +1,6 @@
 package edu.chalmers.pickuapp.app.model;
 
+import android.util.Log;
 import edu.chalmers.pickuapp.app.events.*;
 
 public class DriverSetRoute extends Sequence {
@@ -12,22 +13,9 @@ public class DriverSetRoute extends Sequence {
 
 	public void processEvent(Event event) {
 		
-		//ON OK BUTTON
-		//unsleep main thread, which will resume execute()
-		if(event instanceof DriverPickedRouteEvent) {
-			Date date = new Date(((DriverPickedRouteEvent)event).getDate());
-			RouteData routeData = new RouteData(((DriverPickedRouteEvent)event).getRouteData());
-			
-			//Give routeData to nextSequence (matchmaker)
-			DriverMatchmaker driverMatchmaker = (DriverMatchmaker) getSequence(DriverMatchmaker.class);
-			driverMatchmaker.insert(routeData, date);
-			
-			//Set nextSequence to be matchmaker
-			nextSequence = driverMatchmaker;
-
-			//This sequence is done now, nothing more to do here
-			isDone = true;
-		}
+		if (event instanceof SetRouteEvent){
+            route = new RouteData(((SetRouteEvent) event).getRoute());
+        }
 	}
 
     @Override
