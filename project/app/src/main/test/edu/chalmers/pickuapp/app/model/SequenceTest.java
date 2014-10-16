c19d27491d7e25d5f1eec942f934cc5b6301fab5
Elvira Jonsson <elviraj@student.chalmers.se>
2014-10-16 13:50:13 +0200

    Added SequenceTest

---
+++ project/app/src/main/test/edu/chalmers/pickuapp/app/model/SequenceTest.java
@@ -0,0 +1,40 @@
+package edu.chalmers.pickuapp.app.model;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SequenceTest extends Assert {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNextSequence() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSequence() throws Exception {
+
+    }
+
+    @Test
+    public void testSetSequencesSource() throws Exception {
+
+    }
+
+    @Test
+    public void testOnStart() throws Exception {
+
+    }
+
+    @Test
+    public void testIsDone() throws Exception {
+
+    }
+}
