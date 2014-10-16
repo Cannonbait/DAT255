cde8e342bedd128087ce4f6fd30204447ba5964e
Tejp <tejpbit@hotmail.se>
2014-10-16 15:07:55 +0200

    Added enhancment to be able to display coordinate at start of activity

--- project/app/src/main/java/edu/chalmers/pickuapp/app/MapsActivity.java
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/MapsActivity.java
@@ -9,9 +9,17 @@ import com.google.android.gms.maps.model.*;
 
 /**
  * Created by Tejp on 2014-10-13.
+ *
+ *
+ * How to use this activity to display a position:
+ *
+ * Intent intent = new Intent(this, MapsActivity.class);
+ * intent.putExtra(MapsActivity.INTENT_START_CORDS_KEY, new double[]{50.0, 30.0}); //change theese coordinates accordingly
+ * startActivity(intent);
  */
 public class MapsActivity extends FragmentActivity {
 
+    public static final String INTENT_START_CORDS_KEY = "start_cords";
     public static final String INTENT_CORDS_KEY = "coords";
     private LatLng latLng;
 
@@ -20,23 +28,32 @@ public class MapsActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        final GoogleMap map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        Intent intent = getIntent();
+        double[] posArr = intent.getDoubleArrayExtra(INTENT_START_CORDS_KEY);
 
-        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-                MapsActivity.this.latLng = latLng;
-                map.clear();
-                map.addMarker(new MarkerOptions().position(latLng));
-            }
-        });
+        final GoogleMap map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        if (posArr != null ) {
+            map.addMarker(new MarkerOptions().position(new LatLng(posArr[0], posArr[1])));
+        } else {
+            map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                @Override
+                public void onMapClick(LatLng latLng) {
+                    MapsActivity.this.latLng = latLng;
+                    map.clear();
+                    map.addMarker(new MarkerOptions().position(latLng));
+                }
+            });
+        }
+
     }
 
     @Override
     public void onBackPressed() {
-        Intent returnIntent = getIntent();
-        returnIntent.putExtra(MapsActivity.INTENT_CORDS_KEY, new double[]{latLng.latitude, latLng.longitude});
-        setResult(RESULT_OK, returnIntent);
+        if (latLng != null) {
+            Intent returnIntent = getIntent();
+            returnIntent.putExtra(MapsActivity.INTENT_CORDS_KEY, new double[]{latLng.latitude, latLng.longitude});
+            setResult(RESULT_OK, returnIntent);
+        }
 
         super.onBackPressed();
     }
