856379a96df5730473625b2a7500a338f85ff27f
Joakim Thorén <joakimthoren93@gmail.com>
2014-10-20 19:23:00 +0200

    fixed #112 fixed #113

---
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/events/SetDefaultRouteDataEvent.java
@@ -0,0 +1,14 @@
+package edu.chalmers.pickuapp.app.events;
+
+import edu.chalmers.pickuapp.app.model.Coordinate;
+import edu.chalmers.pickuapp.app.model.Date;
+import edu.chalmers.pickuapp.app.model.RouteData;
+
+public class SetDefaultRouteDataEvent implements Event {
+    
+	public RouteData routeData;
+
+    public SetDefaultRouteDataEvent(RouteData routeData) {
+    	routeData = new RouteData(routeData);
+    }
+}
--- project/app/src/main/java/edu/chalmers/pickuapp/app/model/DriverSetRoute.java
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/model/DriverSetRoute.java
@@ -2,6 +2,7 @@ package edu.chalmers.pickuapp.app.model;
 
 import android.util.Log;
 import edu.chalmers.pickuapp.app.events.*;
+import java.util.*;
 
 public class DriverSetRoute extends Sequence {
 
@@ -9,7 +10,32 @@ public class DriverSetRoute extends Sequence {
 
 	public DriverSetRoute() {
 		super();
+
+        Calendar cal = Calendar.getInstance();
+        route = new RouteData(
+            new Coordinate(0, 0),
+            new Coordinate(0, 0), 
+            new Date(
+                cal.get(Calendar.YEAR), 
+                cal.get(Calendar.MONTH), 
+                cal.get(Calendar.DAY_OF_MONTH), 
+                cal.get(Calendar.HOUR_OF_DAY), 
+                cal.get(Calendar.MINUTE),
+                cal.get(Calendar.SECOND)),
+            new Date(
+                0,
+                0,
+                0,
+                0,
+                0,
+                0));
 	}
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.INSTANCE.reportEvent(new SetDefaultRouteDataEvent(route));
+    }
 
 	public void processEvent(Event event) {
 		
