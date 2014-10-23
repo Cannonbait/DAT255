4ba7758a2eae4753fa7d0cfc75aa322fd3eed9ef
Cannonbait <ivar_busig@hotmail.com>
2014-10-23 15:00:39 +0200

    Removed logging and fix formatting

--- project/app/src/main/java/edu/chalmers/pickuapp/app/server/MockNetwork.java
+++ project/app/src/main/java/edu/chalmers/pickuapp/app/server/MockNetwork.java
@@ -13,7 +13,6 @@ public class MockNetwork implements Network{
 
     public void startDriverMatchmaking(final RouteData data){
         //If we want to modify the match do this here
-        Log.i("MockNetwork", "Starting matchmaking");
         this.data = new RouteData(data);
         sendPacket(new DriverMatchFoundEvent(this.data, this.data.getStartDate()));
     }
@@ -41,12 +40,9 @@ public class MockNetwork implements Network{
             public void run(){
                 try {
                     this.sleep(5000);
-                    Log.i("Thread", "HERPDERPDERP");
                     EventBus.INSTANCE.reportEvent(event);
                     EventBus.INSTANCE.reportEvent(new ForwardClickedEvent());
-                } catch (InterruptedException e){
-
-                }
+                } catch (InterruptedException e){}
             }
         };
         t.start();
